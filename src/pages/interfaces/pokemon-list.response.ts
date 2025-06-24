d4930f48be7fd56ecfbeda72b12d82ada81fa9c8
Ranie2704 <abadcastro424@gmail.com>
2025-06-24 11:52:59 -0600

    HTTP protocols

---
+++ src/pages/interfaces/pokemon-list.response.ts
@@ -0,0 +1,15 @@
+export interface PokemonListResponse{
+
+    count: number;
+    next: string;
+    previous: null;
+    results: Result[];
+
+}
+
+export interface Result{
+
+    name: string;
+    url: string;
+
+}
